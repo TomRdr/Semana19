62c3d9a72fee49cad1fee2ace0cbd9482639680a
TomRdr <72907542+TomRdr@users.noreply.github.com>
2021-05-31 01:29:09 -0600

    Add files via upload

---
+++ Semana19/controllers/employeeController.js
@@ -0,0 +1,110 @@
+const express = require('express');
+var router = express.Router();
+const mongoose = require('mongoose');
+const Employee = mongoose.model('Employee');
+
+router.get('/', (req, res) => {
+    res.render("employee/addOrEdit", {
+        viewTitle: "Insertar nuevo registro"
+    });
+});
+
+router.post('/', (req, res) => {
+    if (req.body._id == '')
+        insertRecord(req, res);
+        else
+        updateRecord(req, res);
+});
+
+
+function insertRecord(req, res) {
+    var employee = new Employee();
+    employee.nombre = req.body.nombre;
+    employee.email = req.body.email;
+    employee.telefono = req.body.telefono;
+    employee.ciudad = req.body.ciudad;
+    employee.save((err, doc) => {
+        if (!err)
+            res.redirect('employee/list');
+        else {
+            if (err.name == 'ValidationError') {
+                handleValidationError(err, req.body);
+                res.render("employee/addOrEdit", {
+                    viewTitle: "Insertar registro",
+                    employee: req.body
+                });
+            }
+            else
+                console.log('Error durante el registro : ' + err);
+        }
+    });
+}
+
+function updateRecord(req, res) {
+    Employee.findOneAndUpdate({ _id: req.body._id }, req.body, { new: true }, (err, doc) => {
+        if (!err) { res.redirect('employee/list'); }
+        else {
+            if (err.name == 'ValidationError') {
+                handleValidationError(err, req.body);
+                res.render("employee/addOrEdit", {
+                    viewTitle: 'Actualizar Registro',
+                    employee: req.body
+                });
+            }
+            else
+                console.log('Error durante la actualización del registro : ' + err);
+        }
+    });
+}
+
+
+router.get('/list', (req, res) => {
+    Employee.find((err, docs) => {
+        if (!err) {
+            res.render("employee/list", {
+                list: docs
+            });
+        }
+        else {
+            console.log('Error al recuperar la lista de empleados:' + err);
+        }
+    });
+});
+
+
+function handleValidationError(err, body) {
+    for (field in err.errors) {
+        switch (err.errors[field].path) {
+            case 'nombre':
+                body['nombreError'] = err.errors[field].message;
+                break;
+            case 'email':
+                body['emailError'] = err.errors[field].message;
+                break;
+            default:
+                break;
+        }
+    }
+}
+
+router.get('/:id', (req, res) => {
+    Employee.findById(req.params.id, (err, doc) => {
+        if (!err) {
+            res.render("employee/addOrEdit", {
+                viewTitle: "Actulizado",
+                employee: doc
+            });
+        }
+    });
+});
+
+router.get('/delete/:id', (req, res) => {
+    Employee.findByIdAndRemove(req.params.id, (err, doc) => {
+        if (!err) {
+            res.redirect('/employee/list');
+        }
+        else { console.log('Error en la eliminación :' + err); }
+    });
+});
+
+module.exports = router;
---
+++ Semana19/server.js
@@ -0,0 +1,26 @@
+//servidor de la app
+require('./models/db')
+
+const express = require('express')
+const path = require('path')
+const exphbs = require('express-handlebars')
+const bodyparser = require('body-parser');
+const employeeController =require('./controllers/employeeController');
+
+var app = express();
+app.use(bodyparser.urlencoded({
+    extended: true
+}));
+app.use(bodyparser.json());
+app.set('views', path.join(__dirname, '/views/'));
+app.engine('hbs', exphbs({extname: 'hbs', defaultLayout: 'mainLayout', layoutsDir: __dirname + '/views/layouts/'}));
+app.set('view engine', 'hbs')
+
+
+//ruta del server
+app.listen(3000, ()=>{
+    console.log('el server esta en el puerto : 3000')
+});
+
+//conoxion con el router
+app.use('/employee', employeeController);
